dba0abab3f415b67c061830a0dca7ce93b099ddc
Carlo Pietrobattista <irony00100@gmail.com>
2023-06-13 04:13:50 +0700

    First commit

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ if [ ! -e $PWD/logs/wp-debug.log ] ; then
     touch $PWD/logs/wp-debug.log
 fi
 
+docker-compose -f docker-compose.yml up -d
+
+chown -R www-data:www-data $PWD/logs
+chown -R www-data:www-data $PWD/www
+chown -R www-data:www-data $PWD/config
+
 find $PWD/www -type d -exec chmod -R 775 {} \;
 find $PWD/www -type f -exec chmod -R 664 {} \;
 find $PWD/logs -type f -exec chmod -R 664 {} \;
 find $PWD/config -type f -exec chmod -R 664 {} \;
-
-chown -R www-data:www-data $PWD/www
-chown -R www-data:www-data $PWD/logs
-chown -R www-data:www-data $PWD/config
-
-docker-compose -f docker-compose.yml up -d
