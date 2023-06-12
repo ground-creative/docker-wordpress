351921c96dfe0c19f803a56aa6eb4b367e82aea1
Carlo Pietrobattista <irony00100@gmail.com>
2023-06-13 04:13:50 +0700

    First commit

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ fi
 
 docker-compose -f docker-compose.yml up -d
 
+if [ ! -e $PWD/www/README.md ] ; then
+    touch $PWD/www/README.md
+fi
+
 chown -R www-data:www-data $PWD/logs
 chown -R www-data:www-data $PWD/www
 chown -R www-data:www-data $PWD/config
