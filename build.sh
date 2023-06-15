f8563e553c5c6f29ab3da61579b19985cd3de3fb
Carlo Pietrobattista <irony00100@gmail.com>
2023-06-15 07:28:03 +0700

    updated readme file

--- build.sh
+++ build.sh
@@ -15,6 +15,8 @@ chown -R www-data:www-data $PWD/www
 chown -R www-data:www-data $PWD/config
 
 find $PWD/www -type d -exec chmod -R 775 {} \;
+find $PWD/logs -type d -exec chmod -R 775 {} \;
+find $PWD/config -type d -exec chmod -R 775 {} \;
 find $PWD/www -type f -exec chmod -R 664 {} \;
 find $PWD/logs -type f -exec chmod -R 664 {} \;
 find $PWD/config -type f -exec chmod -R 664 {} \;
